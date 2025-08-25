ebdcf0bed61c8bb2f8b8f9da37d9c9ded78319a2
Andre <andrepontde@gmail.com>
2025-08-25 12:17:37 +0100

    Added new File and folder structure creation implementation     - was using processBuilder but that can cause conflicts in different machines     - Now using Files, Paths, and Path classes to ensure compatibility

--- myowngit/src/main/java/dev/andrepontde/initbuilder.java
+++ myowngit/src/main/java/dev/andrepontde/initbuilder.java
@@ -1,31 +1,61 @@
 package dev.andrepontde;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
 
 public class initbuilder {
     ProcessBuilder pb;
 
 
     public initbuilder (String folderName) {
-        pb = new ProcessBuilder("mkdir", folderName);
         try {
-            Process process = pb.start();
-            pb = new ProcessBuilder("mkdir", folderName+"/pivco");
-            process = pb.start();
-            pb = new ProcessBuilder("mkdir", folderName+"/pivco/hooks", 
-            folderName+"/pivco/objects", folderName+"/pivco/refs", folderName+"/pivco/info");
-            // pb = new ProcessBuilder("echo", folderName+"config", );
+            Path repoPath = Paths.get(folderName);
+            Files.createDirectory(repoPath);
+
+            Path pivcoPath = repoPath.resolve(".pivco");
+            //Add attributes to the path later
+            Files.createDirectory(pivcoPath);
+
+            Set<String> folders = Set.of( "hooks", "objects", "refs", "info");
+
+            for (String fName : folders) {
+                Files.createDirectory(pivcoPath.resolve(fName));
+            }
+
+            Set<String> files = Set.of("HEAD", "config", "description");
+            for (String fName : files) {
+                Files.createFile(pivcoPath.resolve(fName));
+            }
+
+            System.out.println("Initialized pivco repository");
+            //Añadir los files que se necesitan tambien
         }catch (IOException e){
             System.out.println("Init could not run: " + e.getMessage());
         }
     }
     
     public initbuilder() { 
-        pb = new ProcessBuilder("mkdir", ".pivco");
-        
         try {
-            Process process = pb.start();
-            System.out.println(process);
+            Path pivcoPath = Paths.get("").resolve(".pivco");
+            //Add attributes to the path later
+            Files.createDirectory(pivcoPath);
+
+            Set<String> folders = Set.of( "hooks", "objects", "refs", "info");
+
+            for (String fName : folders) {
+                Files.createDirectory(pivcoPath.resolve(fName));
+            }
+
+            Set<String> files = Set.of("HEAD", "config", "description");
+            for (String fName : files) {
+                Files.createFile(pivcoPath.resolve(fName));
+            }
+
+            System.out.println("Initialized pivco repository");
+            //Añadir los files que se necesitan tambien
         }catch (IOException e){
             System.out.println("Init could not run: " + e.getMessage());
         }

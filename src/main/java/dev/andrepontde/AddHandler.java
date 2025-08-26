becd0bea237cf7010f9d7bb020547c3208bb6397
Andre <andrepontde@gmail.com>
2025-08-26 11:18:58 +0100

    Add single file hashing and implementation completed

--- src/main/java/dev/andrepontde/AddHandler.java
+++ src/main/java/dev/andrepontde/AddHandler.java
@@ -35,14 +35,32 @@ public class AddHandler {
         try {
             digest = MessageDigest.getInstance("SHA-1");
             
+            //Getting File content and creating blob header with standard git format
             byte [] fContent = Files.readAllBytes(fPath);
-            byte [] hashBytes =  digest.digest(fContent);
+            String headerString = "blob " + fContent.length + "\0";
+            byte[] headerBytes = headerString.getBytes();
+
+            //Actual hashing using SHA-1, first git iteration of hashing systems
+            digest.update(headerBytes);
+            digest.update(fContent);
+            byte [] hashBytes = digest.digest();
+
+            //Get the hexFormat of the hashBytes to get the folder name and title for your blob
             String hashString = HexFormat.of().formatHex(hashBytes);
 
-            blobPath = blobBuilder(fContent, hashString);
+            //Copy the byte arrays into a single one to build the content inside the blob in 
+            //a separate function
+            byte[] fullBlob = new byte[headerBytes.length + fContent.length];
+            System.arraycopy(headerBytes, 0, fullBlob, 0, headerBytes.length);
+            System.arraycopy(fContent, 0, fullBlob, headerBytes.length, fContent.length);    
+
+            blobPath = blobBuilder(fullBlob, hashString);
 
             // System.out.println(hashString);
             // System.out.println(fContent);
+
+            //TODO - add idex reference!!!
+            //TODO - make a serializable map to write into the index instead of just a plain text
 
         } catch (java.security.NoSuchAlgorithmException | java.io.IOException e) {
             System.out.println("Could not hash file: " + e.getMessage());
@@ -63,7 +81,6 @@ public class AddHandler {
             Files.write(blob, content);
             bPath = blob.toString();
 
-            //TODO - add idex reference!!!
 
         } catch (java.io.IOException e) {
             System.out.println("Could not create object: " + e.getMessage());
